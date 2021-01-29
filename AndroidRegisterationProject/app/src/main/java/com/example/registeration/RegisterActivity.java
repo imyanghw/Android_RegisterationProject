a3f5ae5736c5661ecbeb80a1fa97f312a482c8e6
imyanghw <imyang3163@naver.com>
2021-01-29 23:49:10 +0900

    안드로이드 수강신청 프로젝트

--- AndroidRegisterationProject/app/src/main/java/com/example/registeration/RegisterActivity.java
+++ AndroidRegisterationProject/app/src/main/java/com/example/registeration/RegisterActivity.java
@@ -3,8 +3,12 @@ package com.example.registeration;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -16,8 +20,23 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        spinner = (Spinner)findViewById(R.id.majorSpinner);
-        adapter = ArrayAdapter.createFromResource(this, R.array.major, android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter); //spinner로 전공 목록을 나열
+        final Spinner mSpinner = findViewById(R.id.majorSpinner);
+        String[] models = getResources().getStringArray(R.array.major);
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(getBaseContext(), R.layout.support_simple_spinner_dropdown_item, models);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        mSpinner.setAdapter(adapter); //spinner로 전공 목록을 나열해줌
+
+        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                mSpinner.getSelectedItem().toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
