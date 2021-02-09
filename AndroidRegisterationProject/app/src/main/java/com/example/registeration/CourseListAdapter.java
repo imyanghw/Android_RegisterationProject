d70ea819a1bd1dfeb152698fda57be884c3a82f7
imyanghw <imyang3163@naver.com>
2021-02-09 09:37:27 +0900

    안드로이드 수강신청 프로젝트

--- AndroidRegisterationProject/app/src/main/java/com/example/registeration/CourseListAdapter.java
+++ AndroidRegisterationProject/app/src/main/java/com/example/registeration/CourseListAdapter.java
@@ -33,11 +33,17 @@ public class CourseListAdapter extends BaseAdapter {
         private Context context;
         private List<Course> courseList;
         private Fragment parent;
+        private String userID = MainActivity.userID; //메인 Activity에 public 형태의 userID 값을 가져옴
+        private Schedule schedule = new Schedule();
+        private List<Integer> courseIDList;
 
         public CourseListAdapter(Context context, List<Course> courseList, Fragment parent) {
             this.context = context;
             this.courseList = courseList;
             this.parent = parent;
+            schedule = new Schedule();
+            courseIDList = new ArrayList<Integer>();
+            new BackgroundTask().execute();
         }
 
         @Override
@@ -94,40 +100,137 @@ public class CourseListAdapter extends BaseAdapter {
 
                 @Override
                 public void onClick(View v) {
-                    String userID = MainActivity.userID;
-                    Response.Listener<String> responseListener = new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            try {
-                                JSONObject jsonResponse = new JSONObject(response);
-                                boolean success = jsonResponse.getBoolean("success");
-                                if(success) {
-                                    androidx.appcompat.app.AlertDialog.Builder builder = new androidx.appcompat.app.AlertDialog.Builder(parent.getActivity());
-                                    //아이디가 빈공간일때 예외를 처리해 다시 작성하게끔 해줌
-                                    AlertDialog dialog = builder.setMessage("강의가 추가되었습니다.")
-                                            .setPositiveButton("확인", null)
-                                            .create();
-                                    dialog.show();
-                                }
-                                else {
-                                    androidx.appcompat.app.AlertDialog.Builder builder = new AlertDialog.Builder(parent.getActivity());
-                                    //아이디가 빈공간일때 예외를 처리해 다시 작성하게끔 해줌
-                                    AlertDialog dialog = builder.setMessage("강의 추가에 실패하였습니다.")
-                                            .setNegativeButton("확인", null)
-                                            .create();
-                                    dialog.show();
-                                } //중복체크에 실패했다면(사용할 수 없는 아이디라면)
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            } //response를 다시 받을 수 있도록
-                        }
-                    };
-                    AddRequest addRequest = new  AddRequest(userID, courseList.get(i).getCourseID() + "", responseListener);
-                    RequestQueue queue = Volley.newRequestQueue(parent.getActivity());
-                    queue.add(addRequest);
+                    boolean validate = false;
+                    validate = schedule.validate(courseList.get(i).getCourseTime()); //현재 시간표에 강의를 넣음으로써 유효성을 검증
+                    if (!alreadyIn(courseIDList, courseList.get(i).getCourseID())) { //만약 자기가 이미 신청한 강의 ID 값이면
+                        AlertDialog.Builder builder = new AlertDialog.Builder(parent.getActivity());
+                        AlertDialog dialog = builder.setMessage("이미 추가한 강의입니다.")
+                                .setPositiveButton("다시 시도", null)
+                                .create();
+                        dialog.show();
+                    }
+                    else if (validate == false)
+                    {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(parent.getActivity());
+                        AlertDialog dialog = builder.setMessage("시간표가 중복됩니다.")
+                                .setPositiveButton("다시 시도", null)
+                                .create();
+                        dialog.show();
+                    }
+                    else {
+                        Response.Listener<String> responseListener = new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                try {
+                                    JSONObject jsonResponse = new JSONObject(response);
+                                    boolean success = jsonResponse.getBoolean("success");
+                                    if (success) {
+                                        androidx.appcompat.app.AlertDialog.Builder builder = new androidx.appcompat.app.AlertDialog.Builder(parent.getActivity());
+                                        //아이디가 빈공간일때 예외를 처리해 다시 작성하게끔 해줌
+                                        AlertDialog dialog = builder.setMessage("강의가 추가되었습니다.")
+                                                .setPositiveButton("확인", null)
+                                                .create();
+                                        dialog.show();
+                                        courseIDList.add(courseList.get(i).getCourseID());
+                                        schedule.addSchedule(courseList.get(i).getCourseTime());
+                                    } else {
+                                        androidx.appcompat.app.AlertDialog.Builder builder = new AlertDialog.Builder(parent.getActivity());
+                                        //아이디가 빈공간일때 예외를 처리해 다시 작성하게끔 해줌
+                                        AlertDialog dialog = builder.setMessage("강의 추가에 실패하였습니다.")
+                                                .setNegativeButton("확인", null)
+                                                .create();
+                                        dialog.show();
+                                    } //중복체크에 실패했다면(사용할 수 없는 아이디라면)
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                } //response를 다시 받을 수 있도록
+                            }
+                        };
+                        AddRequest addRequest = new AddRequest(userID, courseList.get(i).getCourseID() + "", responseListener);
+                        RequestQueue queue = Volley.newRequestQueue(parent.getActivity());
+                        queue.add(addRequest);
+                    }
                 }
             });
+
             return v;
+        }
+
+
+
+    class BackgroundTask extends AsyncTask<Void, Void, String>
+    {
+        String target;
+
+        @Override
+        protected void onPreExecute(){
+            try {
+                target = "https://imyang3163.cafe24.com/ScheduleList.php?userID=" + URLEncoder.encode(userID, "UTF-8");
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            try{
+                URL url=new URL(target);
+                HttpURLConnection httpURLConnection=(HttpURLConnection)url.openConnection();
+                InputStream inputStream=httpURLConnection.getInputStream();
+                BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(inputStream));
+                String temp;
+                StringBuilder stringBuilder=new StringBuilder();
+                while((temp=bufferedReader.readLine())!=null)
+                {
+                    stringBuilder.append(temp+"\n");
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return stringBuilder.toString().trim();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        public void onProgressUpdate(Void... values){
+            super.onProgressUpdate();
+        }
+
+        @Override
+        public void onPostExecute(String result)
+        {
+            try{
+                JSONObject jsonObject=new JSONObject(result);
+                JSONArray jsonArray=jsonObject.getJSONArray("response");
+                int count=0;
+                String courseProfessor, courseTime;
+                int courseID;
+                while(count<jsonArray.length()){
+                    JSONObject object=jsonArray.getJSONObject(count);
+                    courseID=object.getInt("courseID");
+                    courseProfessor=object.getString("courseProfessor");
+                    courseTime=object.getString("courseTime");
+                    courseIDList.add(courseID); //현재 해당 사용자가 가지고 있는 모든 시간표 데이터에 있는 강의 ID 값이 담김
+                    schedule.addSchedule(courseTime);
+                    count++;
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+        public boolean alreadyIn(List<Integer> courseIDList, int item) { //현재 해당하는 courseID 값이 이미 들어가 있는 상태라면
+            for(int i=0; i<courseIDList.size(); i++)
+            {
+                if(courseIDList.get(i) == item){
+                    return false;
+                }
+            }
+            return true;
         }
     }
 
--- AndroidRegisterationProject/app/src/main/java/com/example/registeration/Schedule.java
+++ AndroidRegisterationProject/app/src/main/java/com/example/registeration/Schedule.java
@@ -1,15 +1,18 @@
 package com.example.registeration;
 
+import android.content.Context;
+import android.widget.TextView;
+
 public class Schedule {
 
-    private String monday[]=new String[14];
-    private String tuesday[]=new String[14];
-    private String wednesday[]=new String[14];
-    private String thursday[]=new String[14];
-    private String friday[]=new String[14];
+    private String monday[] = new String[10];
+    private String tuesday[] = new String[10];
+    private String wednesday[] = new String[10];
+    private String thursday[] = new String[10];
+    private String friday[] = new String[10];
 
     public Schedule() {
-        for (int i = 0; i < 14; i++) {
+        for (int i = 0; i < 10; i++) {
             monday[i] = "";
             tuesday[i] = "";
             wednesday[i] = "";
@@ -17,6 +20,7 @@ public class Schedule {
             friday[i] = "";
         }
     }
+
     public void addSchedule(String scheduleText) { //schedule 정보를 담는 특정 데이터를 파싱해서 강의정보 배열에 넣어줌
         int temp;
         //월:[3][4][5] 화:[4][5]
@@ -34,6 +38,293 @@ public class Schedule {
                     monday[Integer.parseInt(scheduleText.substring(startPoint + 1, endPoint))] = "수업";
                 }
             }
+        }
+
+        if ((temp = scheduleText.indexOf("화")) > -1) {
+            temp += 2;
+            int startPoint = temp;
+            int endPoint = temp;
+            for (int i = temp; i < scheduleText.length() && scheduleText.charAt(i) != ':'; i++) {
+                if (scheduleText.charAt(i) == '[') {
+                    startPoint = i;
+                }
+
+                if (scheduleText.charAt(i) == ']') {
+                    endPoint = i;
+                    tuesday[Integer.parseInt(scheduleText.substring(startPoint + 1, endPoint))] = "수업";
+                }
+            }
+        }
+
+        if ((temp = scheduleText.indexOf("수")) > -1) {
+            temp += 2;
+            int startPoint = temp;
+            int endPoint = temp;
+            for (int i = temp; i < scheduleText.length() && scheduleText.charAt(i) != ':'; i++) {
+                if (scheduleText.charAt(i) == '[') {
+                    startPoint = i;
+                }
+
+                if (scheduleText.charAt(i) == ']') {
+                    endPoint = i;
+                    wednesday[Integer.parseInt(scheduleText.substring(startPoint + 1, endPoint))] = "수업";
+                }
+            }
+        }
+
+        if ((temp = scheduleText.indexOf("목")) > -1) {
+            temp += 2;
+            int startPoint = temp;
+            int endPoint = temp;
+            for (int i = temp; i < scheduleText.length() && scheduleText.charAt(i) != ':'; i++) {
+                if (scheduleText.charAt(i) == '[') {
+                    startPoint = i;
+                }
+
+                if (scheduleText.charAt(i) == ']') {
+                    endPoint = i;
+                    thursday[Integer.parseInt(scheduleText.substring(startPoint + 1, endPoint))] = "수업";
+                }
+            }
+        }
+
+        if ((temp = scheduleText.indexOf("금")) > -1) {
+            temp += 2;
+            int startPoint = temp;
+            int endPoint = temp;
+            for (int i = temp; i < scheduleText.length() && scheduleText.charAt(i) != ':'; i++) {
+                if (scheduleText.charAt(i) == '[') {
+                    startPoint = i;
+                }
+
+                if (scheduleText.charAt(i) == ']') {
+                    endPoint = i;
+                    friday[Integer.parseInt(scheduleText.substring(startPoint + 1, endPoint))] = "수업";
+                }
+            }
+        }
+    }
+
+    public boolean validate(String scheduleText) {
+        if (scheduleText.equals("")) { //새롭게 추가하려는 수강 신청 날짜의 데이터가 현재 schedule 데이터에 중복되지 않는지 체크           return true;
+            return true;
+        }
+        int temp;
+        if ((temp = scheduleText.indexOf("월")) > -1) {
+            temp += 2;
+            int startPoint = temp;
+            int endPoint = temp;
+            for (int i = temp; i < scheduleText.length() && scheduleText.charAt(i) != ':'; i++) {
+                if (scheduleText.charAt(i) == '[') {
+                    startPoint = i;
+                }
+
+                if (scheduleText.charAt(i) == ']') {
+                    endPoint = i;
+                    if (!monday[Integer.parseInt(scheduleText.substring(startPoint + 1, endPoint))].equals("")) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        if ((temp = scheduleText.indexOf("화")) > -1) {
+            temp += 2;
+            int startPoint = temp;
+            int endPoint = temp;
+            for (int i = temp; i < scheduleText.length() && scheduleText.charAt(i) != ':'; i++) {
+                if (scheduleText.charAt(i) == '[') {
+                    startPoint = i;
+                }
+
+                if (scheduleText.charAt(i) == ']') {
+                    endPoint = i;
+                    if (!tuesday[Integer.parseInt(scheduleText.substring(startPoint + 1, endPoint))].equals("")) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        if ((temp = scheduleText.indexOf("수")) > -1) {
+            temp += 2;
+            int startPoint = temp;
+            int endPoint = temp;
+            for (int i = temp; i < scheduleText.length() && scheduleText.charAt(i) != ':'; i++) {
+                if (scheduleText.charAt(i) == '[') {
+                    startPoint = i;
+                }
+
+                if (scheduleText.charAt(i) == ']') {
+                    endPoint = i;
+                    if (!wednesday[Integer.parseInt(scheduleText.substring(startPoint + 1, endPoint))].equals("")) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        if ((temp = scheduleText.indexOf("목")) > -1) {
+            temp += 2;
+            int startPoint = temp;
+            int endPoint = temp;
+            for (int i = temp; i < scheduleText.length() && scheduleText.charAt(i) != ':'; i++) {
+                if (scheduleText.charAt(i) == '[') {
+                    startPoint = i;
+                }
+
+                if (scheduleText.charAt(i) == ']') {
+                    endPoint = i;
+                    if (!thursday[Integer.parseInt(scheduleText.substring(startPoint + 1, endPoint))].equals("")) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        if ((temp = scheduleText.indexOf("금")) > -1) {
+            temp += 2;
+            int startPoint = temp;
+            int endPoint = temp;
+            for (int i = temp; i < scheduleText.length() && scheduleText.charAt(i) != ':'; i++) {
+                if (scheduleText.charAt(i) == '[') {
+                    startPoint = i;
+                }
+
+                if (scheduleText.charAt(i) == ']') {
+                    endPoint = i;
+                    if (!friday[Integer.parseInt(scheduleText.substring(startPoint + 1, endPoint))].equals("")) {
+                        return false; //공백이 아니라면 어떠한 데이터가 들어가 있다는 소리이기 때문에 해당 시간표는 중복되어서 들어갈 수 없음
+                    }
+                }
+            }
+        }
+        return true;
+    }
+    //시간표에 추가되어서 보여지는 부분
+    public void addSchedule(String scheduleText, String courseTitle, String courserProfessor) {
+        String professor;
+        if (courserProfessor.equals("")) {
+            professor = "";
+        } else {
+            professor = "(" + courserProfessor + ")";
+        }
+        int temp;
+        //월:[3][4][5] 화:[4][5]
+        if ((temp = scheduleText.indexOf("월")) > -1) {
+            temp += 2;
+            int startPoint = temp;
+            int endPoint = temp;
+            for (int i = temp; i < scheduleText.length() && scheduleText.charAt(i) != ':'; i++) {
+                if (scheduleText.charAt(i) == '[') {
+                    startPoint = i;
+                }
+
+                if (scheduleText.charAt(i) == ']') {
+                    endPoint = i;
+                    monday[Integer.parseInt(scheduleText.substring(startPoint + 1, endPoint))] = courseTitle + professor;
+                }
+            }
+        }
+
+        if ((temp = scheduleText.indexOf("화")) > -1) {
+            temp += 2;
+            int startPoint = temp;
+            int endPoint = temp;
+            for (int i = temp; i < scheduleText.length() && scheduleText.charAt(i) != ':'; i++) {
+                if (scheduleText.charAt(i) == '[') {
+                    startPoint = i;
+                }
+
+                if (scheduleText.charAt(i) == ']') {
+                    endPoint = i;
+                    tuesday[Integer.parseInt(scheduleText.substring(startPoint + 1, endPoint))] = courseTitle + professor;
+                }
+            }
+        }
+
+        if ((temp = scheduleText.indexOf("수")) > -1) {
+            temp += 2;
+            int startPoint = temp;
+            int endPoint = temp;
+            for (int i = temp; i < scheduleText.length() && scheduleText.charAt(i) != ':'; i++) {
+                if (scheduleText.charAt(i) == '[') {
+                    startPoint = i;
+                }
+
+                if (scheduleText.charAt(i) == ']') {
+                    endPoint = i;
+                    wednesday[Integer.parseInt(scheduleText.substring(startPoint + 1, endPoint))] = courseTitle + professor;
+                }
+            }
+        }
+
+        if ((temp = scheduleText.indexOf("목")) > -1) {
+            temp += 2;
+            int startPoint = temp;
+            int endPoint = temp;
+            for (int i = temp; i < scheduleText.length() && scheduleText.charAt(i) != ':'; i++) {
+                if (scheduleText.charAt(i) == '[') {
+                    startPoint = i;
+                }
+
+                if (scheduleText.charAt(i) == ']') {
+                    endPoint = i;
+                    thursday[Integer.parseInt(scheduleText.substring(startPoint + 1, endPoint))] = courseTitle + professor;
+                }
+            }
+        }
+
+        if ((temp = scheduleText.indexOf("금")) > -1) {
+            temp += 2;
+            int startPoint = temp;
+            int endPoint = temp;
+            for (int i = temp; i < scheduleText.length() && scheduleText.charAt(i) != ':'; i++) {
+                if (scheduleText.charAt(i) == '[') {
+                    startPoint = i;
+                }
+
+                if (scheduleText.charAt(i) == ']') {
+                    endPoint = i;
+                    friday[Integer.parseInt(scheduleText.substring(startPoint + 1, endPoint))] = courseTitle + professor;
+                }
+            }
+        }
+    }
+
+    public void setting(TextView[] monday, TextView[] tuesday, TextView[] wednesday, TextView[] thursday, TextView[] friday, Context context)
+    { //해당 강의목록을 보여줄 수 있도록 세팅
+        for(int i=0; i<10; i++){
+            if(!this.monday[i].equals("")) //특정한 강의가 배열에 들어가있지 않다면
+            {
+                monday[i].setText(this.monday[i]);
+                monday[i].setBackgroundColor(context.getResources().getColor(R.color.colorPrimaryLight));
+            }
+
+            if(!this.tuesday[i].equals(""))
+            {
+                tuesday[i].setText(this.tuesday[i]);
+                tuesday[i].setBackgroundColor(context.getResources().getColor(R.color.colorPrimaryLight));
+            }
+
+            if(!this.wednesday[i].equals(""))
+            {
+                wednesday[i].setText(this.wednesday[i]);
+                wednesday[i].setBackgroundColor(context.getResources().getColor(R.color.colorPrimaryLight));
+            }
+
+            if(!this.thursday[i].equals(""))
+            {
+                thursday[i].setText(this.thursday[i]);
+                thursday[i].setBackgroundColor(context.getResources().getColor(R.color.colorPrimaryLight));
+            }
+
+            if(!this.friday[i].equals(""))
+            {
+                friday[i].setText(this.friday[i]);
+                friday[i].setBackgroundColor(context.getResources().getColor(R.color.colorPrimaryLight));
+            }
+
         }
     }
 }
